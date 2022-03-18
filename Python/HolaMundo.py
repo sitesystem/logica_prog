f459ef38a396cc241c0074141134b28850a9ff95
sitesystem <l_aloaxa05@hotmail.com>
2022-03-17 19:49:42 -0600

    Actualización

---
+++ Python/HolaMundo.py
@@ -0,0 +1,7 @@
+# COMENTARIO: Ejemplo 1 Saludo
+# Los comentarios sirven para documentar y aclarar cada parte
+# del programa en Python
+
+print("HOLA MUNDO")
+print('EDUARDO GONZÁLEZ BERNAL');
+print(10+20)
